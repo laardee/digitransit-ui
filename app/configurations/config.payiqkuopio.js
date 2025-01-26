a9f3076d32d1447752c4b86e14506b1cc8b3dd87
Eetu <4726921+laardee@users.noreply.github.com>
2025-01-26 23:07:06 +0200

    Hide ticket price

--- app/configurations/config.payiqkuopio.js
+++ app/configurations/config.payiqkuopio.js
@@ -9,6 +9,7 @@ delete parentConfig.ticketPurchaseLink;
 export default configMerger(parentConfig, {
   CONFIG,
   ticketLink: 'https://www.payiq.net/kuopio',
+  showTicketPrice: false,
   appBarLink: {
     name: 'PayiQ - Kuopio',
     href: 'https://www.payiq.net/kuopio',
