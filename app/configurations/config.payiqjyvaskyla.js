2cd72d3564d6aff8dd8b571d2527b066e6c18d77
Eetu <4726921+laardee@users.noreply.github.com>
2025-02-24 12:18:52 +0200

    Remove myyntipisteet

--- app/configurations/config.payiqjyvaskyla.js
+++ app/configurations/config.payiqjyvaskyla.js
@@ -7,6 +7,12 @@ const parentConfig = require('./config.jyvaskyla').default;
 delete parentConfig.ticketPurchaseLink;
 delete parentConfig.ticketLink;
 
+if (parentConfig?.geoJson?.layers) {
+  parentConfig.geoJson.layers = parentConfig.geoJson.layers.filter(
+    layer => layer.name.fi.toLowerCase() !== 'myyntipisteet',
+  );
+}
+
 export default configMerger(parentConfig, {
   CONFIG,
   // indexPath: 'jyvaskyla/',
